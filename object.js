4ea4151cde1d98d928bd10f466f8564a07f87cd4
Ocskó Lilla <168168776+olilla92@users.noreply.github.com>
2025-09-10 13:48:03 +0200

    Update object.js
    Kész a teljesen jól működő program.

--- object.js
+++ object.js
@@ -1,8 +1,15 @@
-const tanulok = [];
-const tanulo = {
-    name: "John Doe",
-    email: "john.doe@example.com"
-}
-tanulok.push(tanulo)
+import input from "./input.js";
 
-console.log(tanulok)
+const adat = await input("Hány adatot szeretnél megadni? ");
+
+const diakok = [];
+let szamlalo = 0;
+do {
+  const nev = await input("Név: ");
+  const email = await input("Email: ");
+  diakok.push(`Név: ${nev} Email: ${email}`);
+  szamlalo++;
+} while (szamlalo != adat);
+
+console.log("A tanulók adatai: ");
+diakok.forEach((diak) => console.log(diak));
