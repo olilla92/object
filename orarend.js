6b34864b5900755f89f9d1a00abc067b56a9e162
Ocskó Lilla <168168776+olilla92@users.noreply.github.com>
2025-09-10 14:00:53 +0200

    Add files via upload

---
+++ orarend.js
@@ -0,0 +1,125 @@
+const hetfo = [
+  {
+    oraszam: 1,
+    tantargy: "matematika",
+  },
+  {
+    oraszam: 2,
+    tantargy: "nyelvtan",
+  },
+  {
+    oraszam: 3,
+    tantargy: "biológia",
+  },
+  {
+    oraszam: 4,
+    tantargy: "testnevelés",
+  },
+  {
+    oraszam: 5,
+    tantargy: "földrajz",
+  },
+];
+
+const kedd = [
+  {
+    oraszam: 1,
+    tantargy: "testnevelés",
+  },
+  {
+    oraszam: 2,
+    tantargy: "fizika",
+  },
+  {
+    oraszam: 3,
+    tantargy: "fizika",
+  },
+  {
+    oraszam: 4,
+    tantargy: "kémia",
+  },
+  {
+    oraszam: 5,
+    tantargy: "matematika",
+  },
+];
+
+const szerda = [
+  {
+    oraszam: 1,
+    tantargy: "irodalom",
+  },
+  {
+    oraszam: 2,
+    tantargy: "nyelvtan",
+  },
+  {
+    oraszam: 3,
+    tantargy: "biológia",
+  },
+  {
+    oraszam: 4,
+    tantargy: "testnevelés",
+  },
+  {
+    oraszam: 5,
+    tantargy: "techinka",
+  },
+];
+
+const csut = [
+  {
+    oraszam: 1,
+    tantargy: "matematika",
+  },
+  {
+    oraszam: 2,
+    tantargy: "tesi",
+  },
+  {
+    oraszam: 3,
+    tantargy: "biológia",
+  },
+  {
+    oraszam: 4,
+    tantargy: "földrajz",
+  },
+  {
+    oraszam: 5,
+    tantargy: "kémia",
+  },
+];
+
+const pentek = [
+  {
+    oraszam: 1,
+    tantargy: "fizika",
+  },
+  {
+    oraszam: 2,
+    tantargy: "nyelvtan",
+  },
+  {
+    oraszam: 3,
+    tantargy: "matematika",
+  },
+  {
+    oraszam: 4,
+    tantargy: "testnevelés",
+  },
+  {
+    oraszam: 5,
+    tantargy: "testnevelés",
+  },
+];
+
+console.log("Hétfő");
+hetfo.forEach((item) => console.log(item));
+console.log("Kedd");
+kedd.forEach((item) => console.log(item));
+console.log("Szerda");
+szerda.forEach((item) => console.log(item));
+console.log("Csütörtök");
+csut.forEach((item) => console.log(item));
+console.log("Péntek");
+pentek.forEach((item) => console.log(item));
